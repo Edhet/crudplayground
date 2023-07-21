3ee9e6749f98b64eb808d66566ae75842b907ab6
Eduardo P <edtvports@gmail.com>
2023-07-21 18:19:17 -0300

    Marked repositories with annotations

--- src/main/java/com/edhet/crudplayground/repositories/StudentRepositoryMongo.java
+++ src/main/java/com/edhet/crudplayground/repositories/StudentRepositoryMongo.java
@@ -2,9 +2,11 @@ package com.edhet.crudplayground.repositories;
 
 import com.edhet.crudplayground.models.StudentMongo;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface StudentRepositoryMongo extends MongoRepository<StudentMongo, String> {
 
     Optional<StudentMongo> findByEmail(String email);
--- src/main/java/com/edhet/crudplayground/repositories/StudentRepositoryPostgres.java
+++ src/main/java/com/edhet/crudplayground/repositories/StudentRepositoryPostgres.java
@@ -3,9 +3,11 @@ package com.edhet.crudplayground.repositories;
 import com.edhet.crudplayground.models.StudentPostgres;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
+@Repository
 public interface StudentRepositoryPostgres extends CrudRepository<StudentPostgres, Long> {
 
     @Query("SELECT s FROM StudentPostgres s WHERE s.email = :email")
